c799b27c30e38ce2c3addfc3bc28bc939179bcbf
João Guilherme Souza Couto <168004754+JnGuilherme07@users.noreply.github.com>
2025-09-02 12:06:42 -0300

    2-9

---
+++ ADS 02-09-25 (arrey e tupla)/arrey_e_tuplas.py
@@ -0,0 +1,32 @@
+#arrwy é uma lista mutavel:
+
+array = [1,2,3]
+array_misto = [1, "texto", 3.14, True, [1,2,3]]
+
+#------------------------------------------------------------#
+
+#tuplo é uma lista imutavel:
+
+tupla = (1,2,3)
+tupla_misto = (1, "texto", 3.14, True, (1,2,3))
+
+#------------------------------------------------------------#
+
+#Para ver o array ou tupla:
+print(array)
+
+#para achar um item expecifico no arrey:
+print(array[1])
+
+#Para procurar um grupo de itens em uma lista:
+print(array[1:3])
+
+#------------------------------------------------------------#
+
+#Operadores comuns
+
+#O IN verifica se a condição é verdadeira:
+print(20 in array)
+    #retorna false ou true
+
+#------------------------------------------------------------#
